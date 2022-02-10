31bdb2e48d171373bc61467cca3c5e18c618516b
Mariia Dryha <m.pishchanska@gmail.com>
2022-02-10 23:57:50 +0300

    add modal window

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -4,25 +4,168 @@ import { galleryItems } from './gallery-items.js';
 const galleryContainer = document.querySelector("div.gallery");
 
 
-
- const createList = galleryItems.map(item => {
-  
-      let createListItem = document.createElement('li');
-    createListItem = `<div class="gallery__item">
-  <a class="gallery__link" href='${item.original}'>
-    <img
-      class="gallery__image"
-      src="${item.preview}"
-      data-source='${item.original}'
-      alt="${item.description}"
-    />
-  </a>
+const makeGalleryItemMarkup = ({ preview, original, description }) => {
+  return `<div class="gallery__item">
+  <a class="gallery__link" href='${original}'>
+  <img
+  class="gallery__image"
+   src="${preview}"
+  data-source='${original}'
+   alt="${description}"
+  />
+ </a>
 </div>`;
+}
+
+const makeGalleryMarkup = galleryItems.map(makeGalleryItemMarkup).join('');
+ galleryContainer.insertAdjacentHTML('afterbegin', makeGalleryMarkup);
+
+
+
+
+galleryContainer.addEventListener('click', onClick);
+
+function onClick(event) {
+
+  if (event.target.nodeName !== 'IMG') {
+    return;
+  }
+  
+  event.target.src = event.target.dataset.source;
+  event.preventDefault();
+
+  openModal( event.target.dataset.source);
+}
+
+function openModal(src) {
+  const instance = basicLightbox.create(`
+    <div class="modal">
+         <img src="${src}" width="1200" height="800"/>
+    </div>
+`)
+
+instance.show()
+}
+
+
+
+
+
+
+// const createList = galleryItems.map(item => {
+  
+//   let createListItem = document.createElement('li');
+//   createListItem = `<div class="gallery__item">
+//   <a class="gallery__link" href='${item.original}'>
+//     <img
+//       class="gallery__image"
+//       src="${item.preview}"
+//       data-source='${item.original}'
+//       alt="${item.description}"
+//     />
+//   </a>
+// </div>`;
         
-galleryContainer.insertAdjacentHTML('afterbegin', createListItem);
-console.log(galleryContainer);
+//   galleryContainer.insertAdjacentHTML('afterbegin', createListItem);
+//   console.log(galleryContainer);
       
-})
+// }).join('');
+
+
+
+
+
+
+
+// const createList = galleryItems.map(item => {
+  
+//          let createListItem = document.createElement('li');
+//     createListItem = `<li class="gallery__item">
+//   <a
+//     class="gallery__link"
+//     href="${item.original}"
+//     data-lightbox='${item.original}'
+//   >
+//     <img
+//       class="gallery__image"
+//       src="${item.preview}" 
+//       alt="${item.description}"
+//     />
+//   </a>
+// </li>`;
+        
+// galleryContainer.insertAdjacentHTML('afterbegin', createListItem);
+// console.log(galleryContainer);
+      
+// })
+
+
+
+
+
+
+
+
+
+
+// const createList = galleryItems.map(item => {
+
+
+//   const instance = basicLightbox.create(`
+//     <img class="gallery__image"
+//      src="${item.preview}" 
+//      alt="${item.description}">
+// `)
+
+//   instance.show()
+//   galleryContainer.insertAdjacentHTML('afterbegin', instance);
+//   console.log(galleryContainer);
+
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// galleryContainer.addEventListener('click', onClick);
+
+// function onClick (event){
+// if(e)
+// }
+
+
+
+
+
+
+
+
+
+
 
 
 
